70d2db5fe50d58dc2d1d128411facee2bddfc25d
Lim Chia Hau <rootlch@users.noreply.github.com>
2015-06-17 17:33:20 +0800

    Update adblock.js

--- adblock.js
+++ adblock.js
@@ -26,5 +26,8 @@ function regexAdMatch(url) {
 }
 
 function domainAdMatch(host) {
-  return AD_DOMAIN_BLACKLIST.some((elem) => true);// dnsDomainIs(host, elem)
+//dnsDomainIs(host, elem)
+  return AD_DOMAIN_BLACKLIST.some(function (elem) {
+    return true;
+  });
 }
